10c0fd9e0c197d20933c7f13c647bcad07773f0a
Linnea <linneagear@gmail.com>
2020-08-13 19:42:36 -0700

    Added search function and render via the employeeCard

--- src/App.js
+++ src/App.js
@@ -2,24 +2,44 @@
 import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import Form from "./components/Form";
 import employees from "./employees.json";
 
 class App extends Component {
- // Setting this.state.employees to the employees json array
- state = {
-    employees
-  };
 
-  // Map over this.state.employees and render a EmployeeCard component for each employee object
+  constructor() {
+    super();
+
+    // set initial search to an empty string
+    this.state = {
+      search: ""
+    };
+  }
+
+  // capture the "event" of the search box on change and set the value equal to the state variable
+  searchSpace = (event) => {
+    let keyword = event.target.value;
+    this.setState({ search: keyword })
+  }
+
+
   render() {
-    return (
-      <Wrapper>
-      
-        <Title>Employee Directory</Title>
-          <Form />
-        {this.state.employees.map(employee => (
+    const styleInfo = {
+      paddingRight: '10px'
+    }
+
+    // filter data based on the search variable which includes what is in this.state.search
+    const items = employees.filter((data) => {
+      if (this.state.search == null)
+        return data
+      else if (data.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        data.location.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        data.occupation.toLowerCase().includes(this.state.search.toLowerCase())) {
+        return data
+      }
+      // feeds the above data to the map function
+    }).map(employee => {
+      return (
+        <Wrapper>
           <EmployeeCard
             id={employee.id}
             key={employee.id}
@@ -27,11 +47,18 @@ class App extends Component {
             image={employee.image}
             occupation={employee.occupation}
             location={employee.location}
+            number={employee.number}
           />
-        ))}
-      </Wrapper>
-    
-    );
+        </Wrapper>
+      )
+    })
+
+    return (
+      <div>
+        <input type="text" placeholder="Enter item to be searched" onChange={(e) => this.searchSpace(e)} />
+        {items}
+      </div>
+    )
   }
 }
 
